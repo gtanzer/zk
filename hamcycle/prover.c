72abab310b1724219b28f02fa17fe546e18cda7f
Garrett Tanzer <gtanzer@college.harvard.edu>
2018-03-20 22:36:38 -0400

    skeleton

---
+++ hamcycle/prover.c
@@ -0,0 +1,7 @@
+// Garrett Tanzer
+// zk hamiltonian cycle prover
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
---
+++ hamcycle/verifier.c
@@ -0,0 +1,7 @@
+// Garrett Tanzer
+// zk hamiltonian cycle verifier
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
